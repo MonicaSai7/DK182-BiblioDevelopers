4eaab0f57cb3af8e0c67758535a5084bae09e7e9
MonicaSai7 <monica.sai7@gmail.com>
2020-08-01 14:21:46 +0530

    AWS Cognito js SDK config files

---
+++ templates/js/config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_022cKWmQ3', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-1', // e.g. us-east-2
+		clientId: '736tevohdk8vdccpv7sufvvjsn' //is this used anywhere?
+    },
+};
