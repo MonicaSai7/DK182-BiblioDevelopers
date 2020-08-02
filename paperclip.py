d931741f377ec427241aca3f8f5c8fa13b4318f0
preethi chamarthi <cjpreethi@gmail.com>
2020-08-02 11:08:25 +0530

    with cognito

--- paperclip.py
+++
@@ -1,5 +0,0 @@
-import pyperclip
-from datetime import datetime
-pyperclip.copy('The text to be copied to the clipboard.')
-res = pyperclip.paste()
-print(res)
--- templates/js/config.js
+++
@@ -1,7 +0,0 @@
-window._config = {
-    cognito: {
-        userPoolId: 'us-east-1_022cKWmQ3', // e.g. us-east-2_uXboG5pAb
-        region: 'us-east-1', // e.g. us-east-2
-		clientId: '736tevohdk8vdccpv7sufvvjsn' //is this used anywhere?
-    },
-};
