8736159a0cd4e57c81612dece36b00119b76d5d4
MonicaSai7 <monica.sai7@gmail.com>
2020-08-02 10:34:20 +0530

    Update user pool config

--- static/js/config.js
+++ static/js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_nY5hBCB0X', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_CpDJFXUTA', // e.g. us-east-2_uXboG5pAb
         region: 'us-east-1', // e.g. us-east-2
-		clientId: '1vbe527areoe3lrb6rvkmefg9v' //is this used anywhere?
+		clientId: '47uc7thrd4lh346ub9cavtp6u9' //is this used anywhere?
     },
 };
